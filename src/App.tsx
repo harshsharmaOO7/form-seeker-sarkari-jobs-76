3001f45bff56198a2eb2f1868d773f2305bfdbaa
harshsharmaOO7 <79790245+harshsharmaOO7@users.noreply.github.com>
2025-06-08 13:34:12 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -7,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import CategoryPage from "./pages/CategoryPage";
 import JobDetails from "./pages/JobDetails";
+import JobForms from "./pages/JobForms";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Disclaimer from "./pages/Disclaimer";
@@ -24,6 +24,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/category/:categoryName" element={<CategoryPage />} />
           <Route path="/job/:jobId" element={<JobDetails />} />
+          <Route path="/job-forms" element={<JobForms />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/disclaimer" element={<Disclaimer />} />
